e7e39319e7b1ebffab88099f0932b73d85a59d7a
KhodamIblis <jodinurfarkhani04@gmail.com>
2023-11-24 18:19:31 +0700

    membuat fungsi untuk menyimpan codingan ke DB

--- pythonDB_060.py
+++ pythonDB_060.py
@@ -11,3 +11,30 @@ def HasilPrediksi(biologi, fisika, inggris):
         return 'Bahasa'
     else:
         return 'hasil belum dapat diprediksi'
+
+def simpan_data_ke_sqlLite(nama_Siswa, biologi, fisika, inggris, prediksi_Fakultas):
+    # membuka atau membuat database SQLite
+    conn = sqlite3.connect("Prediksi_KelasB.db")
+    cursor = conn.cursor()
+
+    # membuat table jika belum ada
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nilai_Siswa
+        (nama_Siswa TEXT,
+         biologi INTEGER, 
+         fisika INTEGER,
+         inggris INTEGER,
+         prediksi_Fakultas TEXT)
+    ''')
+
+    cursor.execute('''
+        INSERT INTO nilai_Siswa
+        (nama_Siswa,
+         biologi,
+         fisika,
+         inggris,
+         prediksi_Fakultas) VALUES (?,?,?,?,?)
+    ''', (nama_Siswa, biologi, fisika, inggris,prediksi_Fakultas))
+
+    conn.commit()
+    conn.close()
