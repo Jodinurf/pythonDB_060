a804debecfa3f093f894321ab0c0aaa0a1fa27a9
KhodamIblis <jodinurfarkhani04@gmail.com>
2023-11-24 18:18:39 +0700

    membuat fungsi HasilPrediksi dengan 3 parameter

--- pythonDB_060.py
+++ pythonDB_060.py
@@ -2,3 +2,12 @@ import tkinter as tk
 import sqlite3
 from tkinter import messagebox
 
+def HasilPrediksi(biologi, fisika, inggris):
+    if biologi > fisika and biologi > inggris:
+        return 'Kedokteran'
+    elif fisika > biologi and fisika > inggris:
+        return 'Teknik'
+    elif inggris > biologi and fisika:
+        return 'Bahasa'
+    else:
+        return 'hasil belum dapat diprediksi'
