7420e388adfc9c91aba7151cdae7db30c09daa95
KhodamIblis <jodinurfarkhani04@gmail.com>
2023-11-24 18:20:48 +0700

    membuat fungsi show yg akan dipanggil sbg tampilan

--- pythonDB_060.py
+++ pythonDB_060.py
@@ -38,3 +38,28 @@ def simpan_data_ke_sqlLite(nama_Siswa, biologi, fisika, inggris, prediksi_Fakult
 
     conn.commit()
     conn.close()
+
+# Fungsi Untuk Menampilkan
+def show():
+    nama_Siswa = entry_Siswa.get()
+    biologi = int(entry_Biologi.get())
+    fisika = int(entry_Fisika.get())
+    inggris = int(entry_Inggris.get())
+
+    Prediksi = HasilPrediksi(biologi, fisika, inggris)
+
+    hasilSiswa = f"Nama Siswa: {nama_Siswa}"
+    hasil1 = f"Nilai Biologi: {biologi}"
+    hasil2 = f"Nilai Fisika: {fisika}"
+    hasil3 = f"Nilai Bahasa Inggris: {inggris}"
+    hasil3 = f"Hasil Prediksi Fakultas : {Prediksi}"
+
+    label_hasilSiswa.config(text=hasilSiswa)
+    label_hasil1.config(text=hasil1)
+    label_hasil2.config(text=hasil2)
+    label_hasil3.config(text=hasil3)
+
+   
+    frame_hasil.pack()
+    simpan_data_ke_sqlLite(nama_Siswa=nama_Siswa, biologi=biologi, fisika=fisika, inggris=inggris, prediksi_Fakultas=Prediksi)
+    messagebox.showinfo("Info", f"Data tersimpan")
