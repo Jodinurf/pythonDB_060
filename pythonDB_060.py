2feccb8cfd5f2ff2c93301391554df021beadae3
KhodamIblis <jodinurfarkhani04@gmail.com>
2023-11-24 18:22:47 +0700

    membuat tampilan halaman prediksi fakultas pilihan

--- pythonDB_060.py
+++ pythonDB_060.py
@@ -63,3 +63,41 @@ def show():
     frame_hasil.pack()
     simpan_data_ke_sqlLite(nama_Siswa=nama_Siswa, biologi=biologi, fisika=fisika, inggris=inggris, prediksi_Fakultas=Prediksi)
     messagebox.showinfo("Info", f"Data tersimpan")
+
+# Buat Jendela Halaman
+root = tk.Tk()
+root.title("Prediksi Fakultas Pilihan")
+root.geometry("500x500")
+root.resizable(False, False)
+
+# Label Judul
+label_judul = tk.Label(root, text="Prediksi Fakultas Pilihan", font=("Times", 14, "bold"))
+label_judul.pack(pady=20)
+
+# Buat Frame inputan
+frame_input = tk.LabelFrame(root, labelanchor="n", pady=10, padx=10)
+frame_input.pack()
+
+# Label Nama siswa
+label_nama_Siswa = tk.Label(frame_input, text="Nama Siswa: ")
+label_nama_Siswa.grid(row=0, column=0, pady=10)
+entry_Siswa = tk.Entry(frame_input)
+entry_Siswa.grid(row=0, column=1)
+
+# Label Nilai Biologi
+label_Biologi = tk.Label(frame_input, text="Nilai Biologi: ")
+label_Biologi.grid(row=1, column=0, pady=10)
+entry_Biologi = tk.Entry(frame_input)
+entry_Biologi.grid(row=1, column=1)
+
+# Label Fisika
+label_Fisika = tk.Label(frame_input, text="Nilai Fisika: ")
+label_Fisika.grid(row=2, column=0, pady=10)
+entry_Fisika = tk.Entry(frame_input)
+entry_Fisika.grid(row=2, column=1)
+
+# Label Inggris
+label_Inggris = tk.Label(frame_input, text="Nilai Inggris: ")
+label_Inggris.grid(row=3, column=0, pady=10)
+entry_Inggris = tk.Entry(frame_input)
+entry_Inggris.grid(row=3, column=1)
